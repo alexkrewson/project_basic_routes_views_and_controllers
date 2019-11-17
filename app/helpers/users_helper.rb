7ae20259413cf85720c61b974da8d06e73bb7025
alexkrewson <alex.krewson@gmail.com>
2019-11-17 15:58:38 -0800

    messing around with images again

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  # def gravatar_for(user)
+  #   gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  #   gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  #   image_tag("gravatar_url", alt: user.name, class: "gravatar")
+  #   # gravatar_image_tag('junk', :alt => 'Github Default Gravatar', :gravatar => { :default => 'https://assets.github.com/images/gravatars/gravatar-140.png' })
+    
+  #   # puts "gravatar_url #{gravatar_url}"
+  # end
 end
