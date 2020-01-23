73f7293545bcf2cff0e53e765a470202f80b853c
alexkrewson <alex.krewson@gmail.com>
2020-01-23 15:05:18 -0800

    finished 7.3.4 A test for invalid submission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   def new
     # debugger
+    @user = User.new
+
   end
 
   def show
     @user = User.find(params[:id])
-    debugger
+    # debugger
   end
 
 
@@ -13,4 +15,22 @@ class UsersController < ApplicationController
     @users = User.all
     # debugger
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
